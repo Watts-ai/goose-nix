a89c05a2928059f815b8be2cadcdcd0b91265061
ryleu <69326171+ryleu@users.noreply.github.com>
2025-08-25 10:15:51 -0500

    update goose & add LICENSE

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      version = "1.5.0";
+      version = "1.6.0";
     in
     {
       packages.${system}.default = pkgs.stdenv.mkDerivation {
@@ -21,7 +21,7 @@
 
         src = pkgs.fetchurl {
           url = "https://github.com/block/goose/releases/download/stable/goose_${version}_amd64.deb";
-          sha256 = "sha256-RBeThQ7ds9XXnDKN6wl5QGN5gZbf8Bb0WPGE4WM8BxY=";
+          sha256 = "sha256-GaMZgv3yH5DzDqjlUBCgUmCW84BwvrYtWebwxqnEnW8=";
         };
 
         nativeBuildInputs = [
